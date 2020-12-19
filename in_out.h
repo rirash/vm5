1efc049622d7e17e2977f82066ccc0ca896a2b01
rirash <76262273+rirash@users.noreply.github.com>
2020-12-19 23:18:59 +0400

    Update in_out.h

--- in_out.h
+++ in_out.h
@@ -2,8 +2,9 @@
 #define IN_OUT_H
 #include "Command.h"
 
+//ВВОД/ВЫВОД
 class IO : public Command
 {
-	virtual void operator()(Processor& cpu) noexcept override;
+	virtual void operator()(Processor& cpu) noexcept override;	//Непосредственно операция
 };
 #endif//! IN_OUT_H
