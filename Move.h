1c2e743aedcf9da09f9148297ef670c26423e803
rirash <76262273+rirash@users.noreply.github.com>
2020-12-19 22:47:34 +0400

    Update Move.h

--- Move.h
+++ Move.h
@@ -4,17 +4,17 @@
 #include "Processor.h"
 #include "Command.h"
 
-class Load_rr : public Command
+class Load_rr : public Command			//Пересылка регистр-регистр
 {
 	void operator()(Processor& cpu);
 };
 
-class Load_rm : public Command
+class Load_rm : public Command			//Пересылка регистр-память
 {
 	void operator()(Processor& cpu);
 };
 
-class Load_mr : public Command
+class Load_mr : public Command			//Пересылка память-регистр
 {
 	void operator()(Processor& cpu);
 };
