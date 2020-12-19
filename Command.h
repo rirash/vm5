8c5e201b00e1f209829ba4a92638311ac57d0350
rirash <76262273+rirash@users.noreply.github.com>
2020-12-19 22:32:26 +0400

    Update Command.h

--- Command.h
+++ Command.h
@@ -2,25 +2,10 @@
 #define COMMAND_H
 
 class Processor;
-class Command
+class Command							//Абстрактный класс КОМАНДА
 {
 public:
 	virtual void operator() (Processor& cpu) = 0;
 };
-
-template <typename T>
-T op_plus(const T& left, const T& right) { return left + right; }
-template <typename T>
-T op_minus(const T& left, const T& right) { return left - right; }
-template <typename T>
-T op_mul(const T& left, const T& right) { return left * right; }
-template <typename T>
-T op_div(const T& left, const T& right) { return left / right; }
-template <typename T>
-T op_and(const T& left, const T& right) { return left & right; }
-template <typename T>
-T op_or(const T& left, const T& right) { return left | right; }
-template <typename T>
-T op_remainder(const T& left, const T& right) { return left % right; }
 
 #endif//! COMMAND_H
