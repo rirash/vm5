ec3a47f7a81880961cfcd2a87da4c2293aa70429
rirash <76262273+rirash@users.noreply.github.com>
2020-12-19 23:10:32 +0400

    Update fMath.cpp

--- fMath.cpp
+++ fMath.cpp
@@ -1,18 +1,18 @@
 #include "Processor.h"
 #include "fMath.h"
 
-void fMath::change_flag(datatype32 res, Processor& cpu) noexcept
+void fMath::change_flag(datatype32 res, Processor& cpu) noexcept 		//Расстановка флагов
 {
 	if (res.w32.f < 0) cpu.psw.setSF(1);
 	if (fabs(res.w32.f) < 0.000000001) cpu.psw.setZF(1);
 }
 
-void fMath::operator()(Processor& cpu)
+void fMath::operator()(Processor& cpu)						//Как таковая операция
 {
-	cpu.psw.resetFlags();
+	cpu.psw.resetFlags();							//Очищаем флаги
 	datatype32 res;
 	cmd32 com = cpu.get_Command();
-	res.w32.f = calculate(cpu.get_float(com.command16.r1), cpu.get_float(com.command16.r2));
-	cpu.put(res, com.command16.r1);
-	change_flag(res, cpu);
+	res.w32.f = calculate(cpu.get_float(com.command16.r1), cpu.get_float(com.command16.r2)); //Выполняем операцию
+	cpu.put(res, com.command16.r1);						//Результат кладем в первый операнд
+	change_flag(res, cpu);							//Расстановка флагов
 }
